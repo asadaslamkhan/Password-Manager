dd474797c8c50661c10062f8330e6bc207fc806c
Asad Aslam <opworrier@gmail.com>
2025-02-22 14:07:13 +0500

    completed

--- passgen/src/components/Footer.jsx
+++ passgen/src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-green-500'> &lt;</span>
+
+                <span>Password</span><span className='text-green-500'>Manager/&gt;</span>
+
+
+            </div>
+            <div className='flex justify-center items-center'> Created with <img className='w-7 mx-2' src="src/assets/heart.png" alt="" /> by Asadaslam </div>
+        </div>
+    )
 }
 
 export default Footer
--- passgen/src/components/Navbar.jsx
+++ passgen/src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='bg-slate-800 text-white'>
-            <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+            <div className="mycontainer flex justify-between items-center px-4  py-5 h-14">
                 <div className="logo font-bold text-white">
                     <span className="text-green-500">&lt;</span>
                     Password
                     <span className="text-green-500"> Manager/&lt;</span>
-                    </div>
+                </div>
                 {/* <ul>
                     <li className="flex gap-4">
                         <a className="hover:font-bold" href="#">Home</a>
@@ -18,7 +18,7 @@ const Navbar = () => {
                 </ul> */}
                 <button className='text-white bg-green-500 my-5 rounded-2xl flex justify-center items-center'>
                     <img className="w-10 p-1" src="src/assets/github.png" alt="github" />
-                    <span className='font-bold px-2'>GitHub</span>
+                    <a href="https://github.com/asadaslamkhan"> <span  className='font-bold px-2'>GitHub</span></a>
                 </button>
             </div>
         </nav>
